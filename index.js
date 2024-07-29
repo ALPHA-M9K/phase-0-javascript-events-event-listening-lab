caf811ff06977ddb95c62f9d12e3bb72a8f3dcf6
ALPHA ALVIN <alpha.muchemi@student.moringaschool.com>
2024-07-29 13:48:28 +0300

    listening

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+
 function addingEventListener() {
-}
+    const button = document.getElementById('button');
+  
+    if (button) {
+      button.addEventListener('click', function() {
+        alert('I was clicked!');
+      });
+    } 
+  }
+
+  addingEventListener();
+  
+  
